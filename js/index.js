31e78786a0ec604a84c899b268eec421e6f9b55f
IanProefriedt <ianproefriedt@icloud.com>
2023-05-20 15:13:42 -0400

    HW 19 Submission
    The portfolio I was attempting to code was getting to complicated in certain areas. I used the template provided instead. I added the event listener for the button click which logs "Button clicked!". I also added a hover state to the button.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("Your index.js file is loading correctly");
+
+
+var button = document.querySelector('button');
+
+
+button.addEventListener('click', function() {
+ 
+  console.log('Button clicked!');
+  
+});
