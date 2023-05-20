8912767c792b3bb313a7ec01647f6929076063ad
IanProefriedt <ianproefriedt@icloud.com>
2023-05-20 15:15:26 -0400

    Hw 19 Submission
    The button now changes colors to indicate that it was pressed

--- js/index.js
+++ js/index.js
@@ -7,5 +7,6 @@ var button = document.querySelector('button');
 button.addEventListener('click', function() {
  
   console.log('Button clicked!');
+  button.style.backgroundColor = '#FF0000'
   
 });
